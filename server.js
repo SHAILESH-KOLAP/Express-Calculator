d976def8286b0d22e2c1e7f7c96cac03fe5957c4
Shailesh Kolap <skolap22@gmail.com>
2022-05-28 14:00:19 +0530

    Add Backend for calculator Used bodyParser to parse data from HTML

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require("express");
+// Bodyparser allow us to parse data getting from post request
+const bodyParser = require("body-parser");
+
 const app = express();
+// Bodyparser is work with express app
+// BodyParser have few modes bodyParser.text() - get data in text format, bodyParser.json() - get data in json format, bodyParser.urlencoded() - get data that is sent from html form
+// .urlencoded is used for getting data from post request
+// extended: true allow us to view nested object
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get('/',(req, res)=>{
-    res.sendFile(__dirname +'/index.html')
-})
+app.post("/", function (req, res) {
+  let num1 = parseInt(req.body.num1);
+  let num2 = parseInt(req.body.num2);
+  res.send(`${num1} + ${num2} = ${num1 + num2}`);
+  console.log(req.body);
+});
 
-
-app.listen(3000,()=>{
-    console.log('Listening on port 3000')
-})
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
